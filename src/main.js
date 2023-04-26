19275203996b2680d7d58e5c69763ab8dcc223be
Sineway <sineway@yandex.com>
2023-04-26 20:25:32 +0300

    Добавим SortView (сортировка списка)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './views/brief-view.js';
 import './views/add-view.js';
 import './views/filter-view.js';
+import './views/sort-view.js';
 
 /**
  * @type {BriefView}
@@ -17,6 +18,12 @@ const addView = document.querySelector('add-view');
  */
 const filterView = document.querySelector('filter-view');
 
+/**
+ * @type {SortView}
+ */
+const sortView = document.querySelector('sort-view');
+
 briefView.render();
 addView.render();
 filterView.render();
+sortView.render();
--- src/types.js
+++ src/types.js
@@ -7,4 +7,5 @@
  * @typedef {import('./views/brief-view.js').default} BriefView
  * @typedef {import('./views/add-view.js').default} AddView
  * @typedef {import('./views/filter-view.js').default} FilterView
+ * @typedef {import('./views/sort-view.js').default} SortView
  */
