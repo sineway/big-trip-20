5cfc186d3a5d2e2143fe5db2cea46f6fe53359f2
Sineway <sineway@yandex.com>
2023-04-26 19:11:03 +0300

    Добавим AddView (кнопка добавления)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import './views/brief-view.js';
+import './views/add-view.js';
 
 /**
  * @type {BriefView}
  */
 const briefView = document.querySelector('brief-view');
 
+/**
+ * @type {AddView}
+ */
+const addView = document.querySelector('add-view');
+
 briefView.render();
+addView.render();
