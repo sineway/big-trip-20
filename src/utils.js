453751e37fbfb4b38ef767d92646c4b6a6762afc
Sineway <sineway@yandex.com>
2023-05-14 20:43:19 +0300

    Доработаем функцию formatDuration

--- src/utils.js
+++ src/utils.js
@@ -27,8 +27,17 @@ function formatTime(dateTime) {
  */
 function formatDuration(startDateTime, endDateTime) {
   const milliseconds = dayjs(endDateTime).diff(startDateTime);
+  const duration = dayjs.duration(milliseconds);
 
-  return dayjs.duration(milliseconds).format('HH[h] mm[m]');
+  if (duration.days()) {
+    return duration.format('DD[d] HH[h] mm[m]');
+  }
+
+  if (duration.hours()) {
+    return duration.format('HH[h] mm[m]');
+  }
+
+  return duration.format('mm[m]');
 }
 
 class SafeHtml extends String {}
