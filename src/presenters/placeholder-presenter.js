da885c381da84b8632c56dfea55ad9ce3d46086e
Sineway <sineway@yandex.com>
2023-06-03 21:38:01 +0300

    Спрячем плейсхолдер при открытии редактора новой точки маршрута

--- src/presenters/placeholder-presenter.js
+++ src/presenters/placeholder-presenter.js
@@ -38,7 +38,7 @@ class PlaceholderPresenter extends Presenter {
 
       return {
         text: this.textMap[urlParams.filter] ?? this.textMap.everything,
-        isHidden: points.length > 0,
+        isHidden: urlParams.edit === 'draft' || points.length > 0,
       };
     }
 
