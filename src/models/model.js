a54e9dd402eeec889ae83e0d491eb67c58cb3e84
Sineway <sineway@yandex.com>
2023-06-03 17:11:44 +0300

    Научим модель отправлять события

--- src/models/model.js
+++ src/models/model.js
@@ -1,6 +1,16 @@
 /**
  * @abstract
  */
-class Model {}
+class Model extends EventTarget {
+  /**
+   * @param {string} type
+   * @param {any} [detail]
+   */
+  notify(type, detail = null) {
+    const event = new CustomEvent(type, {detail});
+
+    this.dispatchEvent(event);
+  }
+}
 
 export default Model;
