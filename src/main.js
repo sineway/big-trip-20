8b27b6ff0de67bfa78319b01aab32199974500db
Sineway <sineway@yandex.com>
2023-06-03 21:21:39 +0300

    Реализуем блокировку кнопок фильтра

--- src/main.js
+++ src/main.js
@@ -25,7 +25,7 @@ new PlaceholderPresenter(document.querySelector('placeholder-view'), appModel);
 appModel.load().then(() => {
   new BriefPresenter(document.querySelector('brief-view'));
   new AddPresenter(document.querySelector('add-view'));
-  new FilterPresenter(document.querySelector('filter-view'));
+  new FilterPresenter(document.querySelector('filter-view'), appModel);
   new SortPresenter(document.querySelector('sort-view'));
   new ListPresenter(document.querySelector('list-view'), appModel);
   new OverlayPresenter(document.querySelector('overlay-view'), appModel);
--- src/presenters/filter-presenter.js
+++ src/presenters/filter-presenter.js
@@ -1,7 +1,7 @@
 import Presenter from './presenter.js';
 
 /**
- * @extends {Presenter<FilterView>}
+ * @extends {Presenter<FilterView, AppModel>}
  */
 class FilterPresenter extends Presenter {
   /**
@@ -22,7 +22,7 @@ class FilterPresenter extends Presenter {
     const items = types.map((it) => ({
       value: it,
       isSelected: it === filter,
-      isDisabled: false,
+      isDisabled: this.model.getPoints({filter: it}).length === 0,
     }));
 
     return {items};
